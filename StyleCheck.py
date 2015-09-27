f85bf071313e80d1c41f61ac87cc241531081f1e
Evamy <antriksh5235@gmail.com>
2015-09-27 10:27:20 +0530

    stylecheck useless removed

--- StyleCheck.py
+++ StyleCheck.py
@@ -26,23 +26,3 @@ def posgrams(filename):
 	X = count_vect.fit_transform(doc[:-1] for doc in training_data)
 
 	return X.toarray(), count_vect.vocabulary_
-
-def posgrams3(filename):
-	with open(filename,'r') as f:
-		data = f.read()
-
-	data = data.decode('utf-8')
-	bigram_vectorizer = CountVectorizer(ngram_range=(3,3),token_pattern=r'\b\w+\b', min_df=1)
-	analyze = bigram_vectorizer.build_analyzer()
-
-	output = analyze(data)
-
-	training_data = nltk.pos_tag(output)
-
-	# print training_data
-
-	count_vect = CountVectorizer(preprocessor=lambda x:x,tokenizer=lambda x:x)
-	
-	X = count_vect.fit_transform(doc[:-1] for doc in training_data)
-
-	return X.toarray(), count_vect.vocabulary_
