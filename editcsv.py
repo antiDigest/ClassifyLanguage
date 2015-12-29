a4b56ed8d740d78f75be7e891453276ca4d8c9af
Antriksh Agarwal <antriksh5235@gmail.com>
2015-12-29 15:22:37 +0530

    csv to analyse what we have, and we have nothing yet

--- editcsv.py
+++ editcsv.py
@@ -5,8 +5,16 @@ columns = ['Country','Type','Serial','CEFR','Upper/lower/students/teachers/other
 'Spelling','Word Count','Average Sentence Length','Function Word Count','FWratio']
 
 
-for files in os.listdir('Outputs'):
-	# print files
-	data = pd.read_csv('Outputs/'+files,sep=',|[,\(]|\),|\_',names=columns,engine='python')
+# for files in os.listdir('Outputs'):
+	
+data = pd.read_csv('Outputs/Base.csv',names=columns,engine='python')
 
-	data.to_csv('Outputs/Base.csv',header=False,sep=',', mode='a',columns=columns)
+CEFR = []
+for i in data.itertuples():
+	CEFR += [i[4]+'_'+i[5]]
+
+data['Upper/lower/students/teachers/others'] = pd.DataFrame(CEFR)
+
+print data
+
+data.to_csv('Outputs/Base(1).csv',header=False, mode='a',columns=columns)
