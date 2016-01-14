8012f619a34103e63aad4d30ebcc698127909cac
evamy <antriksh5235@gmail.com>
2016-01-14 23:13:47 +0530

    downloading more packages of nltk

--- Structure.py
+++ Structure.py
@@ -1,8 +1,19 @@
 import nltk
 import json
 from nltk.data import load
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
+# tagdict = load('help/tagsets/claws5_tagset.pickle')
+# print json.dumps(tagdict)
 
+data = open('tags.txt')
+k = json.load(data)
+
+tagdict = dict(k)
+
+# print dict(k).keys()
 
 def POS_tag(text):
 	tokens = nltk.word_tokenize(text)
