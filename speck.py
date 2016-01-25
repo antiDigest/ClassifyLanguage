e4b410f1b0ebb2c029c554c652244dfc7ca7ea93
alyakhtar <samurai.aly@gmail.com>
2016-01-24 18:05:21 -0800

    spelling checker updated
    Signed-off-by: alyakhtar <samurai.aly@gmail.com>

--- speck.py
+++ speck.py
@@ -1,46 +1,61 @@
 from stemming.porter2 import stem
 from textblob import TextBlob
 from MinEditDist import dist
-import sys,re
+import sys
+import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-file1 = open('icnale_201302/Categorized/CHN/A20  (N=50)/CHN_PTJ_021_A2_0.txt','r')
-xyz = file1.read().decode("utf-8-sig")
-words = re.split('[\s?\.!,:;]+',xyz)
 
 def check_word(var):
-	a = TextBlob(var)
-	new = a.correct()
+    a = TextBlob(var)
+    new = a.correct()
+    
+    if var == new:
+        print "Word = ",var
+        x = var 
+        with open("bigger.txt") as myfile:
+	        for line in myfile:
+	            y= line.strip()
+	            k = dist(x,y)
+	            if k == 1 or k == 2:
+	            	print y
 
-	if var == new:
-		print var,'Added to dictionary'
-		with open('bigger.txt','a') as w:
-			w.write(var+'\n')
-	else:
-		x = var
-		y = new
-		print 'MinEditDist = {%s} for {%s} and {%s}' % (dist(x,y),x,y)
+    else:
+        x = var
+        y = new
+        print 'MinEditDist = {%s} for {%s} and {%s}' % (dist(x, y), x, y)
+
 
 def check_spelling(var):
-	with open("bigger.txt") as myfile:
-		for line in myfile:
-			if line.strip() == var.lower():
-				return 1
-	return -1
+    with open("bigger.txt") as myfile:
+        for line in myfile:
+            if line.strip() == var.lower():
+                return 1
+    return -1
+
 
 def check(var):
 
-	afs = stem(var)
-	flag = check_spelling(afs.strip())
-	
-	if flag == -1:
-		check_word(var)
+    afs = stem(var)
+    flag = check_spelling(afs.strip())
 
-for word in words:
-	
-	flag = check_spelling(word.strip())
-		
-	if flag == -1:
-		check(word)
+    if flag == -1:
+        if var is not None or var is not ' ':
+            check_word(var)
 
+
+def main():
+    file1 = open('file1.txt', 'r')
+    xyz = file1.read().decode("utf-8-sig")
+    words = re.split('[\s?\.!,:;]+', xyz)
+
+    for word in words:
+
+        flag = check_spelling(word.strip())
+
+        if flag == -1:
+            check(word)
+
+if __name__ == "__main__":
+    main()
