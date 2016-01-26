e5aadfbf808d7b0a2167c4ae3396b2202a667cf8
evamy <antriksh5235@gmail.com>
2016-01-26 16:15:22 +0530

    a bit rearranging a few corrections

--- probability.py
+++ probability.py
@@ -1,102 +1,107 @@
 import glob
 import re
+import time
 
 # 1 for Eng, 2 for Non Eng
-
 # 89057 words in ENS
-
 # 1192362 words in Non ENS
 
-
-class probability:
+class Probability:
 
 	def __init__(self, x, word = None):
 		self.x = x
 		self.y = word
 		if self.y == None:
-			if self.x == 1:
-				prob = 400.0/5600.0
-				print round(prob,3)
-			elif self.x == 2:
-				prob = 5200.0/5600.0
-				print round(prob,3)
-		else: 
-			if self.x == 1:
-				count = 0.0
-				for file in glob.glob("files/Base/*.txt"):
-					a = file.split("\\")
-					if 'E' in a[1] and 'N' in a[1] and 'S' in a[1] :
-						with open(file) as myfile:
-							data = myfile.read().decode('utf-8-sig')
+			print self.single()
+		else:
+			print self.conditional()
+			
+	def single(self):
+		if self.x == 1:
+			prob = 400.0/5600.0
+			return '{0:.5f}'.format(prob)
+		elif self.x == 2:
+			prob = 5200.0/5600.0
+			return '{0:.5f}'.format(prob)
 
-							word = re.split('[\s?\.!,:; ]+', data)[:-1]
+	def conditional(self):
+		if self.x == 1:
+			count = 0.0
+			for f in glob.glob("icnale_201302/Base/*.txt"):
+				if 'ENS' in f:
+					with open(f) as myfile:
+						data = myfile.read().decode('utf-8-sig')
 
-							for check in word:								
-								if check.lower() == self.y:
-									count = count + 1
+						word = re.split('[\s?\.!,:; ]+', data)[:-1]
 
-							myfile.close()	
-				prob = count/89057.0
-				print round(prob,3)
+						for check in word:								
+							if check.lower() == self.y:
+								count = count + 1
 
-			elif self.x == 2:
-				count = 0.0
-				for file in glob.glob("files/Base/*.txt"):
-					a = file.split("\\")
-					if 'E' not in a[1] :
-						with open(file) as myfile:
-							data = myfile.read().decode('utf-8-sig')
+						myfile.close()	
+			prob = count/89057.0
+			return '{0:.5f}'.format(prob)
 
-							word = re.split('[\s?\.!,:; ]+', data)[:-1]
+		elif self.x == 2:
+			count = 0.0
+			for f in glob.glob("icnale_201302/Base/*.txt"):
+				if 'ENS' not in f:
+					with open(f) as myfile:
+						data = myfile.read().decode('utf-8-sig')
 
-							for check in word:								
-								if check.lower() == self.y:
-									count = count + 1
+						word = re.split('[\s?\.!,:; ]+', data)[:-1]
 
-							myfile.close()	
-				prob = count/1192362.0
-				print round(prob,3)
+						for check in word:								
+							if check.lower() == self.y:
+								count = count + 1
+
+						myfile.close()	
+			prob = count/1192362.0
+
+			return '{0:.5f}'.format(prob)
 
 
 # To find No in words in Non ENS Files
-# def count_words_nonENS():   
-# 	i = 0
-# 	count = 0
-# 	for file in glob.glob("files/Base/*.txt"):
-# 		a = file.split("\\")
-# 		if 'E' not in a[1] :
-# 			with open(file) as myfile:
-# 				data = myfile.read().decode('utf-8-sig')
+def count_words_nonENS():   
+	i = 0
+	count = 0
+	for f in glob.glob("icnale_201302/Base/*.txt"):
+		if 'ENS' not in f:
+			with open(f) as myfile:
+				data = myfile.read().decode('utf-8-sig')
 
-# 				word = data.split()
+				word = data.split()
 
-# 				count = count + len(word)
+				count = count + len(word)
 
-# 				myfile.close()	
+				myfile.close()	
 
-# 	return count
+	return count
 
 
 # To find No in words in ENS Files
-# def count_words_ENS():
-# 	i = 0
-# 	count = 0
-# 	for file in glob.glob("files/Base/*.txt"):
-# 		a = file.split("\\")
-# 		if 'E' in a[1] and 'N' in a[1] and 'S' in a[1] :
-# 			with open(file) as myfile:
-# 				data = myfile.read().decode('utf-8-sig')
+def count_words_ENS():
+	i = 0
+	count = 0
+	for f in glob.glob("icnale_201302/Base/*.txt"):
+		if 'ENS' in f:
+			with open(f) as myfile:
+				data = myfile.read().decode('utf-8-sig')
 
-# 				word = data.split()
+				word = data.split()
 
-# 				count = count + len(word)
+				count = count + len(word)
 
-# 				myfile.close()
+				myfile.close()
 
-# 	return count 
-
+	return count 
 
 
-# b = probability(1)
-a = probability(2,"job")
+start = time.time()
 
+Probability(2)
+Probability(2,"job")
+
+end = time.time()
+
+print "time",end-start
