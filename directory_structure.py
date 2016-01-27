5434a1b8eb199b9ae0d428dcb80333a844202438
evamy <antriksh5235@gmail.com>
2016-01-27 20:30:31 +0530

    chhota code for splitting

--- directory_structure.py
+++ directory_structure.py
@@ -1,416 +1,86 @@
 import glob
 import os
-import shutil
+# import shutil
+from shutil import copy
 import random
 
-def main():
-	for file in glob.glob("files/Base/*.txt"):
-		a = file.split("\\")
-		if not os.path.exists("files/Main/"):
-		    os.makedirs("files/Main")
-		path = 'files/Main/%s' %a[1]
-		shutil.copy(file, path)
+var = ['ENS','HKG','PAK','PHL','SIN','CHN','IDN','JPN','KOR','THA','TWN']
 
-def ENS():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('ENS') > -1 :	
-			files.append(a[1])
+def move():
 
-	for i in xrange(280):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
+	if not os.path.exists("icnale_201302/Main/"):
+	    os.makedirs("icnale_201302/Main")
+	if not os.path.exists("icnale_201302/Main/TRAIN"):
+	    os.makedirs("icnale_201302/Main/TRAIN")
+	if not os.path.exists("icnale_201302/Main/DEV"):
+	    os.makedirs("icnale_201302/Main/DEV")
+	if not os.path.exists("icnale_201302/Main/TEST"):
+	    os.makedirs("icnale_201302/Main/TEST")
 
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
+	k = []
+	k1 = []
+	k2 = []
+	k3 = []
 
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
+	for cont in var:
+		i = 0
+		for f in glob.glob("icnale_201302/Base/*.txt"):
+			
+			if cont in f and 'PTJ' in f:
+				k+=[f]
+				if i<70:
+					filepath = f
+					copypath = "icnale_201302/Main/TRAIN/"
+					copy(filepath,copypath)
+					k1 += [f]
+				elif 90>i>=70 and i<90:
+					filepath = f
+					copypath = "icnale_201302/Main/DEV/"
+					copy(filepath,copypath)
+					k2 += [f]
+				elif i>=90 and i<100:
+					filepath = f
+					copypath = "icnale_201302/Main/TEST/"
+					copy(filepath,copypath)
+					k3 += [f]
 
+			elif cont in f and 'SMK' in f:
+				k += [f]
+				if i<70:
+					filepath = f
+					copypath = "icnale_201302/Main/TRAIN/"
+					copy(filepath,copypath)
+					k1 += [f]
+				elif i>=70 and i<90 :
+					filepath = f
+					copypath = "icnale_201302/Main/DEV/"
+					copy(filepath,copypath)
+					k2 += [f]
+				elif i>=90 and i<100:
+					filepath = f
+					copypath = "icnale_201302/Main/TEST/"
+					copy(filepath,copypath)
+					k3 += [f]
 
-def HKG():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('HKG') > -1 :	
-			files.append(a[1])
+			i += 1
 
-	for i in xrange(140):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
+			if i>=100:
+				i=0
 
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(20):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def PAK():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('PAK') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(280):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def PHL():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('PHL') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(280):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-	
-	
-def SIN():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('SIN') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(280):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def CHN():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('CHN') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(560):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(160):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def IDN():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('IDN') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(280):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def JPN():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('JPN') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(560):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(160):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-
-def KOR():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('KOR') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(420):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(120):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(60):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def THA():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('THA') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(560):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(160):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-
-def TWN():
-	files = []
-	for file in glob.glob("files/Main/*.txt"):
-		a = file.split("\\")
-		if a[1].find('TWN') > -1 :	
-			files.append(a[1])
-
-	for i in xrange(280):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TRAIN"):
-			    os.makedirs("files/Main/TRAIN")
-		path = 'files/Main/TRAIN/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(80):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/DEV"):
-			    os.makedirs("files/Main/DEV")
-		path = 'files/Main/DEV/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-	for i in xrange(40):	
-		a = random.randint(0,len(files)-1)
-		source = 'files/Main/%s' %files[a]
-		if not os.path.exists("files/Main/TEST"):
-			    os.makedirs("files/Main/TEST")
-		path = 'files/Main/TEST/%s' %files[a]
-		shutil.copy(source, path)
-		del files[a]
-
-def remove():
-	for file in glob.glob("files/Main/*.txt"):
-		os.remove(file)
+	return len(k), len(k1),len(k2),len(k3)
 
 if __name__ == "__main__":
-main()
-ENS()
-HKG()
-PAK()
-PHL()
-SIN()
-CHN()
-IDN()
-JPN()
-KOR()
-THA()
-TWN()
-remove()
+	print move()
+	# main()
+	# ENS()
+	# HKG()
+	# PAK()
+	# PHL()
+	# SIN()
+	# CHN()
+	# IDN()
+	# JPN()
+	# KOR()
+	# THA()
+	# TWN()
+# remove()
