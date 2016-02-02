5437f22bd8970744142a38ade34620491ae9c417
Evamy <antriksh5235@gmail.com>
2016-02-02 22:07:53 +0530

    Useless removed,added manager,a bit modification and adding sentiment

--- WordCount.py
+++ WordCount.py
@@ -1,30 +1,30 @@
 import re
 
-with open('EnglishAuxiliaryVerbs.txt','r') as f:
+with open('aux/EnglishAuxiliaryVerbs.txt','r') as f:
 	lines = f.read().split('\n')
 	auxverbs = [line for line in lines if not "//" in line] 
 
-with open('EnglishConjunctions.txt','r') as f:
+with open('aux/EnglishConjunctions.txt','r') as f:
 	lines = f.read().split('\n')
 	conjunctions = [line for line in lines if not "//" in line] 
 
-with open('EnglishDeterminers.txt','r') as f:
+with open('aux/EnglishDeterminers.txt','r') as f:
 	lines = f.read().split('\n')
 	determiners = [line for line in lines if not "//" in line] 
 
-with open('EnglishPrepositions.txt','r') as f:
+with open('aux/EnglishPrepositions.txt','r') as f:
 	lines = f.read().split('\n')
 	prepositions = [line for line in lines if not "//" in line] 
 
-with open('EnglishPronouns.txt','r') as f:
+with open('aux/EnglishPronouns.txt','r') as f:
 	lines = f.read().split('\n')
 	pronouns = [line for line in lines if not "//" in line] 
 
-with open('EnglishQuantifiers.txt','r') as f:
+with open('aux/EnglishQuantifiers.txt','r') as f:
 	lines = f.read().split('\n')
 	quantifiers = [line for line in lines if not "//" in line] 
 
-allwords = auxverbs + conjunctions + determiners +prepositions+pronouns+quantifiers
+allwords = auxverbs + conjunctions + determiners + prepositions + pronouns + quantifiers
 # print allwords
 
 def paragraphs(filename):
@@ -35,20 +35,18 @@ def paragraphs(filename):
 	return (sen)
 
 
-def SentenceLength(filename):
-	with open(filename,'r') as f:
-		fl = f.read()
-	words = len(fl.split(' '))
-	sen = len(re.split(r'[.!?]+', fl))
+def SentenceLength(text):
+	sen = len(re.split(r'[\.!?]+', text))
+	words = len(re.split('[?\.!,:; ]+', text)[:-1])
 	return (words/sen)
 
-def WordCount(filename):
-	with open(filename,'r') as f:
-		return len(f.read().split(' '))
+def WordCount(text):
+	# with open(filename,'r') as f:
+	return len(re.split('[\s?\.!,:; ]+', text)[:-1])
 
-def FunctionWordCount(filename):
-	with open(filename,'r') as f:
-		filewords = f.read().split(' ')
+def FunctionWordCount(text):
+	# with open(filename,'r') as f:
+	filewords = re.split('[\s?\.!,:; ]+', text)[:-1]
 	auxcount = 0
 	concount = 0
 	detcount = 0
@@ -74,24 +72,52 @@ def FunctionWordCount(filename):
 	return count
 
 
- # For reference purposes, a succinct description of each class of function words follows.
+ # # For reference purposes, a succinct description of each class of function words follows.
 
- #    Auxiliary Verbs are verbs whose function is to characterize the main verbs they accompany with shades of meaning pertaining to tense and/or modality. Regarding tense, the core meaning of the verb can be modified to express perfect, progressive, or passive voices. Regarding modality, the main verb is altered to denote judgement or opinion in terms of ability, advice, expectation, intention/willingness, likelihood, necessity, permission/prohibition, or degrees of politeness.
+ # #    Auxiliary Verbs are verbs whose function is to characterize the main verbs they accompany 
+ # with shades of meaning pertaining to tense and/or modality. Regarding tense, the core meaning of 
+ # the verb can be modified to express perfect, progressive, or passive voices. Regarding modality,
+ #  the main verb is altered to denote judgement or opinion in terms of ability, advice, expectation, 
+ #  intention/willingness, likelihood, necessity, permission/prohibition, or degrees of politeness.
 
- #    Auxiliary verbs are necessary to form questions and negatives in English. If auxiliary verbs are used only to serve these functions, they are referred to as dummy auxiliaries. Additionally, the auxiliaries 'do', 'does', and 'did' can be inserted preceding the main verb for emphasis. Modal verbs are distinguished from other auxiliary verbs by their inability to function as main verbs and their lack of complete conjugations (infinitive for example).
+ # #    Auxiliary verbs are necessary to form questions and negatives in English. If auxiliary verbs 
+ # are used only to serve these functions, they are referred to as dummy auxiliaries. Additionally, 
+ # the auxiliaries 'do', 'does', and 'did' can be inserted preceding the main verb for emphasis. Modal 
+ # verbs are distinguished from other auxiliary verbs by their inability to function as main verbs and 
+ # their lack of complete conjugations (infinitive for example).
 
- #    Conjunctions are uninflected function words that serve to conjoin words, clauses, phrases, or sentences. There are three basic forms: single word (however), compound (as long as), and correlative (so ... that). In terms of function, conjunctions can be grouped into additive (so, thus), adversative (but, instead), causative (so, because), and temporal (after, then).
+ # #    Conjunctions are uninflected function words that serve to conjoin words, clauses, phrases, 
+ # or sentences. There are three basic forms: single word (however), compound (as long as), and 
+ # correlative (so ... that). In terms of function, conjunctions can be grouped into additive (so, 
+ # 	thus), adversative (but, instead), causative (so, because), and temporal (after, then).
 
- #    Conjunctions are not structural elements in a clause. Rather, they are external elements that establish grammatical relations (coordination, correlation, subordination) between clauses. Certain adverbial and prepositional phrases can also act as conjunctions (subsequently, in addition to that).
+ # #    Conjunctions are not structural elements in a clause. Rather, they are external elements that 
+ # establish grammatical relations (coordination, correlation, subordination) between clauses. Certain
+ #  adverbial and prepositional phrases can also act as conjunctions (subsequently, in addition to that).
 
- #    Determiners are inflected function words employed as noun modifiers and that serve to alter the referents of noun phrases in terms of amount, location, possession, and general versus specific. In terms of form, determiners are simple (two, their, the) or compound (a number of, one half, a little). Also, possessive and demonstrative adjectives are considered determiners.
+ # #    Determiners are inflected function words employed as noun modifiers and that serve to alter 
+ # the referents of noun phrases in terms of amount, location, possession, and general versus specific.
+ #  In terms of form, determiners are simple (two, their, the) or compound (a number of, one half, a 
+ #  	little). Also, possessive and demonstrative adjectives are considered determiners.
 
- #    The determiner class is often divided into articles (a, an, the), determiners (both, neither, whichever), and quantifiers (much, various, little).
+ # #    The determiner class is often divided into articles (a, an, the), determiners (both, neither,
+ #  whichever), and quantifiers (much, various, little).
 
- #    Prepositions are uninflected function words that combine with nouns, pronouns, or noun phrases to form prepositional phrases that can have, in turn, adverbial or adjectival relationships with other words. Prepositions can be simple (as, of) or compound (next to, in view of) forms. In terms of function, at least the following types of preposition can be distinguished: time (until, circa), location (along, amid), logical (since, given), possession (including, pertaining to), and movement (toward, to).
+ # #    Prepositions are uninflected function words that combine with nouns, pronouns, or noun phrases 
+ # to form prepositional phrases that can have, in turn, adverbial or adjectival relationships with other words. Prepositions can be simple (as, of) or compound (next to, in view of) forms. In terms of function, at least the following types of preposition can be distinguished: time (until, circa), location (along, amid), logical (since, given), possession (including, pertaining to), and movement (toward, to).
 
- #    Prepositions can also occur in post position with: nouns (interest in, need for), adjectives (familiar with, sure of), participles (married to, made of), and verbs (give up, look forward). In this situation, the composite can be thought of as a unit.
+ # #    Prepositions can also occur in post position with: nouns (interest in, need for), adjectives 
+ # (familiar with, sure of), participles (married to, made of), and verbs (give up, look forward). In 
+ # this situation, the composite can be thought of as a unit.
 
- #    Pronouns are inflected function words employed in place of nouns or noun phrases. In terms of form, pronouns are simple (nothing, herself) and compound (each other, one another). Also, some pronoun composites are used in relative clauses (all of whom, several of which).
+ # #    Pronouns are inflected function words employed in place of nouns or noun phrases. In terms of 
+ # form, pronouns are simple (nothing, herself) and compound (each other, one another). Also, some 
+ # pronoun composites are used in relative clauses (all of whom, several of which).
 
- #    Pronouns are classified into the following classes: subject personal (I, he, we), object personal (me, him, us), possessive (mine, his, ours), reflexive (myself, himself, ourselves), demonstrative (this, these, such), relative (who, all, that), indefinite (each, anybody, none), reciprocal (each other, one another), and interrogative (how, who, why). Additionally, reflexives also operate as so-called intensive pronouns when they are employed to emphasize an antecedent noun or pronoun (as in, "The boss himself prepared the coffee" or "I myself could not believe it").
+ # #    Pronouns are classified into the following classes: subject personal (I, he, we), object 
+ # personal (me, him, us), possessive (mine, his, ours), reflexive (myself, himself, ourselves), 
+ # demonstrative (this, these, such), relative (who, all, that), indefinite (each, anybody, none), 
+ # reciprocal (each other, one another), and interrogative (how, who, why). Additionally, reflexives 
+ # also operate as so-called intensive pronouns when they are employed to emphasize an antecedent noun
+ #  or pronoun (as in, "The boss himself prepared the coffee" or "I myself could not believe it").
+
---
+++ manage.py
@@ -0,0 +1,108 @@
+import glob
+import os
+from shutil import copy
+import random
+from WordCount import *
+from text_parsing import *
+from speck import *
+from sentiment import Sentiment
+from polarity import Polarity
+import pandas as pd
+import time
+
+var = ['ENS','HKG','PAK','PHL','SIN','CHN','IDN','JPN','KOR','THA','TWN']
+
+def create_train():
+
+	start = time.time()
+
+	header = ['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRPD','RB','RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WPD','WRB','SentenceLength','WordCount','FunctionWordCount','count_errors','Sentiment','Polarity','Result']
+
+	df = pd.DataFrame(columns=header)
+
+	for f in glob.glob("icnale_201302/Main/TRAIN/*.txt"):
+		if 'ENS' in f:
+			myfile = open(f,'r')
+			text = myfile.read()
+			CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+				RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB = parse(text)
+			data = [CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+							RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB,SentenceLength(text),\
+							WordCount(text),FunctionWordCount(text),count_errors(text),Sentiment(text),Polarity(text),1]
+			
+		else:
+			myfile = open(f,'r')
+			text = myfile.read()
+			CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+				RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB = parse(text)
+			data = [CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+							RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB,SentenceLength(text),\
+							WordCount(text),FunctionWordCount(text),count_errors(text),Sentiment(text),Polarity(text),2]
+
+		df = df.append(pd.Series(data, index=header), ignore_index=True)
+		end = time.time()
+
+		if end-start > 60:
+			print df
+			start =time.time()
+
+	df.to_csv('Outputs/train.csv',header=True,index=False)
+
+	test_df = pd.DataFrame(columns=header)
+
+	for f in glob.glob("icnale_201302/Main/TEST/*.txt"):
+		if 'ENS' in f:
+			myfile = open(f,'r')
+			text = myfile.read()
+			CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+				RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB = parse(text)
+			data = [CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+							RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB,SentenceLength(text),\
+							WordCount(text),FunctionWordCount(text),count_errors(text),Sentiment(text),Polarity(text),1]
+			
+		else:
+			myfile = open(f,'r')
+			text = myfile.read()
+			CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+				RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB = parse(text)
+			data = [CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+							RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB,SentenceLength(text),\
+							WordCount(text),FunctionWordCount(text),count_errors(text),Sentiment(text),Polarity(text),2]
+
+		test_df = test_df.append(pd.Series(data, index=header), ignore_index=True)
+		end = time.time()
+
+		if end-start > 60:
+			print test_df
+			start =time.time()
+
+	for f in glob.glob("icnale_201302/Main/DEV/*.txt"):
+		if 'ENS' in f:
+			myfile = open(f,'r')
+			text = myfile.read()
+			CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+				RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB = parse(text)
+			data = [CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+							RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB,SentenceLength(text),\
+							WordCount(text),FunctionWordCount(text),count_errors(text),Sentiment(text),Polarity(text),1]
+			
+		else:
+			myfile = open(f,'r')
+			text = myfile.read()
+			CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+				RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB = parse(text)
+			data = [CC,CD,DT,EX,FW,IN,JJ,JJR,JJS,LS,MD,NN,NNS,NNP,NNPS,PDT,POS,PRP,PRPD,RB,RBR,RBS,\
+							RP,SYM,TO,UH,VB,VBD,VBG,VBN,VBP,VBZ,WDT,WP,WPD,WRB,SentenceLength(text),\
+							WordCount(text),FunctionWordCount(text),count_errors(text),Sentiment(text),Polarity(text),2]
+
+		test_df = test_df.append(pd.Series(data, index=header), ignore_index=True)
+		end = time.time()
+
+		if end-start > 60:
+			print test_df
+			start =time.time()
+
+	test_df.to_csv('Outputs/test.csv',header=True,index=False)
+
+if __name__ == "__main__":
+	create_train()
